335ce19c3ff952cef01ca80be4b1fdd10d9062ba
I4gojs <iagojosesobrinho65@gmai.com>
2024-03-20 07:58:12 -0300

    Formulário

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var botao =document.getElementById("botao");
-botao.addEventListener("click",alertar,false);
-
+var botao = document.getElementById("botao");
+botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
 
-function alertar(event){
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
+function alertar(event) {
     alert("Você clicou no botão!!!" + nome.value);
+
+    saida.innerText = "Nome:" + nome.value +
+        "\n Email:" + email.value + "\n Telefone:" + telefone.value + "\n CEP:"+ CEP.value + "\n Logradouro:" + logradouro.value+ "\n Número:"+ número.value + "\n Complemento:"+ complemento.value+ "\n Cidade:"+ cidade.value + "\n Estado:"+ estado.value;
 }
