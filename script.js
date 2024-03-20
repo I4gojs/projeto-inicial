610075b8a9d091e2931239c23ecdeceff194febd
I4gojs <iagojosesobrinho65@gmai.com>
2024-03-20 08:30:26 -0300

    CEP

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ var email = document.getElementById("email");
 function alertar(event) {
     alert("Você clicou no botão!!!" + nome.value);
 
-    saida.innerText = "Nome:" + nome.value +
-        "\n Email:" + email.value + "\n Telefone:" + telefone.value + "\n CEP:"+ CEP.value + "\n Logradouro:" + logradouro.value+ "\n Número:"+ número.value + "\n Complemento:"+ complemento.value+ "\n Cidade:"+ cidade.value + "\n Estado:"+ estado.value;
+    const url = `https://viacep.com.br/ws/${CEP.value}/json`;
+
+    fetch(url)
+    .then(resposta=>resposta.json())
+     .then(dados=>{
+       logradouro.value = dados.logradouro;
+       bairro.value = dados.bairro;
+       cidade.value = dados.localidade;
+       estado.value = dados.uf;
+     
+       saida.innerText = "Nome:" + nome.value +
+       "\n Email:" + email.value + 
+       "\n Telefone:" + telefone.value +
+        "\n CEP:"+ CEP.value + 
+        "\n Logradouro:" + logradouro.value+ 
+        "\n Número:"+ número.value + 
+        "\n Complemento:"+ complemento.value+ 
+        "\n Cidade:"+ cidade.value + 
+        "\n Estado:"+ estado.value;
+    
+    })
+
+    
 }
