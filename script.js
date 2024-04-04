a3f08f6c5333762e0eeec4cac5efc6be3304d637
I4gojs <iagojosesobrinho65@gmai.com>
2024-04-04 07:48:02 -0300

    Adicionando variáveis

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ var nome = document.getElementById("nome");
 var saida = document.getElementById("saida-de-dados");
 
 var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 
 function alertar(event) {
     alert("Você clicou no botão!!!" + nome.value);
